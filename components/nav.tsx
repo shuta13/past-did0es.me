3f0d44b4a884f9db94b5cd14e16d9c08b1175a1a
shuta13 <hollystarsun@gmail.com>
2020-01-15 15:54:46 +0900

    fix: fixed error of types

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 
 const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
+  { key: 'dummy', href: 'https://zeit.co/now', label: 'ZEIT' },
   { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`
