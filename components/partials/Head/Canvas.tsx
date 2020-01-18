aed7afdb1a1305985489a2d08037bded8139a724
shuta13 <hollystarsun@gmail.com>
2020-01-19 03:45:12 +0900

    fix: 表示改善

--- components/partials/Head/Canvas.tsx
+++ components/partials/Head/Canvas.tsx
@@ -4,14 +4,13 @@ import * as THREE from 'three'
 import useGetWindowSize from '../../hooks/useGetWindowSize'
 
 const Canvas: React.FC = () => {
+  const { width, height } = useGetWindowSize()
+  console.log(width, height)
+
   const init = (canvas: HTMLCanvasElement) => {
     if (!canvas) {
       return;
     }
-
-    // useEffect(() => {
-    //   const { width, height } = useGetWindowSize()
-    // })
     const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true })
     const scene = new THREE.Scene()
     const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000)
@@ -24,6 +23,7 @@ const Canvas: React.FC = () => {
     renderer.setClearColor('#1d1d1d')
     scene.add(cube)
 
+    renderer.setSize(width, height)
     renderer.render(scene, camera)
   }
   return (
