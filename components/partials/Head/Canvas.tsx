35c320716625470ea407ac9f0cff1201e92eca9d
shuta13 <hollystarsun@gmail.com>
2020-01-19 03:55:44 +0900

    feat: アニメーション完成

--- components/partials/Head/Canvas.tsx
+++ components/partials/Head/Canvas.tsx
@@ -1,34 +1,52 @@
-import React, { useEffect, useState } from 'react'
-import * as THREE from 'three'
+import React from 'react'
+import {
+  WebGLRenderer,
+  Scene, 
+  PerspectiveCamera,
+  BoxGeometry, 
+  MeshBasicMaterial,
+  Mesh
+} from 'three'
 
 import useGetWindowSize from '../../hooks/useGetWindowSize'
 
+type OnCanvasLoaded = {
+  renderer: THREE.WebGLRenderer, scene: THREE.Scene, camera: THREE.PerspectiveCamera, cube: THREE.Mesh
+}
+
 const Canvas: React.FC = () => {
   const { width, height } = useGetWindowSize()
-  console.log(width, height)
 
-  const init = (canvas: HTMLCanvasElement) => {
+  const onCanvasLoaded = (canvas: HTMLCanvasElement) => {
     if (!canvas) {
       return;
     }
-    const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true })
-    const scene = new THREE.Scene()
-    const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000)
+    const renderer = new WebGLRenderer({ canvas: canvas, antialias: true })
+    const scene = new Scene()
+    const camera = new PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000)
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshBasicMaterial({ color: 0xff00ff })
-    const cube = new THREE.Mesh(geometry, material)
+    const geometry = new BoxGeometry(1, 1, 1)
+    const material = new MeshBasicMaterial({ color: 0xff00ff })
+    const cube = new Mesh(geometry, material)
 
     camera.position.z = 4
     renderer.setClearColor('#1d1d1d')
     scene.add(cube)
 
     renderer.setSize(width, height)
+    animate({ renderer, scene, camera, cube })
+  }
+
+  const animate = ({ renderer, scene, camera, cube }: OnCanvasLoaded) => {
+    window.requestAnimationFrame(() => animate({ renderer, scene, camera, cube }))
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
     renderer.render(scene, camera)
   }
+
   return (
     <div>
-      <canvas ref={init}></canvas>
+      <canvas ref={onCanvasLoaded}></canvas>
     </div>
   )
 }
