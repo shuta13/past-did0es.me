4b74593183672cecfda47bc06782ade2dab0f19c
shuta13 <hollystarsun@gmail.com>
2020-01-19 17:06:56 +0900

    wip: making font effects

--- components/partials/Head/Canvas.tsx
+++ components/partials/Head/Canvas.tsx
@@ -1,24 +1,74 @@
-import React from 'react'
+import React, { useState, Fragment } from 'react'
 import {
   WebGLRenderer,
   Scene, 
   PerspectiveCamera,
-  BoxGeometry, 
-  MeshBasicMaterial,
+  Clock,
+  TextureLoader,
+  RawShaderMaterial,
+  DoubleSide,
   Mesh
 } from 'three'
 
+const loadFont = require("load-bmfont")
+const createGeometry = require("three-bmfont-text")
+const MDFShader = require("three-bmfont-text/shaders/msdf")
+const fontFile = require('../../../public/static/fonts/PoiretOne-Regular.ttf')
+const fontAtlas = require('../../../public/static/PoiretOne-Regular.png')
+const vertex = require('./shader/vertex.glsl')
+const fragment = require('./shader/fragment.glsl')
+
 import useGetWindowSize from '../../hooks/useGetWindowSize'
 
-type OnCanvasLoaded = {
-  renderer: THREE.WebGLRenderer,
-  scene: THREE.Scene,
-  camera: THREE.PerspectiveCamera,
-  cube: THREE.Mesh
+// types
+type ParamsAnimate = {
+  renderer: THREE.WebGLRenderer
+  scene: THREE.Scene
+  camera: THREE.PerspectiveCamera
+  clock: THREE.Clock
+}
+type ParamsLoader = {
+  renderer: THREE.WebGLRenderer
+  scene: THREE.Scene
+  camera: THREE.PerspectiveCamera
+  texture: any
+  clock: THREE.Clock
+}
+type ParamsInit = {
+  geometry: any
+  texture: any
+  scene: THREE.Scene
+  clock: THREE.Clock
+  camera: THREE.PerspectiveCamera
+  renderer: THREE.WebGLRenderer
+}
+type ParamsCreateMesh = {
+  geometry: any
+  texture: any
+  scene: THREE.Scene
+}
+type ParamsRender = {
+  clock: THREE.Clock
+  scene: THREE.Scene
+  camera: THREE.PerspectiveCamera
+  renderer: THREE.WebGLRenderer
+}
+type ParamsLoadBMF = {
+  scene: THREE.Scene
+  clock: THREE.Clock
+  camera: THREE.PerspectiveCamera
+  renderer: THREE.WebGLRenderer
 }
 
 const Canvas: React.FC = () => {
+  // call custom hook
   const { width, height } = useGetWindowSize()
+
+  // use state
+  const [geometry, setGeometry] = useState()
+  const [loader, setLoader] = useState()
+  const [material, setMaterial] = useState()
+  const [mesh, setMesh] = useState()
 
   const onCanvasLoaded = (canvas: HTMLCanvasElement) => {
     if (!canvas) {
@@ -27,23 +77,70 @@ const Canvas: React.FC = () => {
     const renderer = new WebGLRenderer({ canvas: canvas, antialias: true })
     const scene = new Scene()
     const camera = new PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000)
-
-    const geometry = new BoxGeometry(1, 1, 1)
-    const material = new MeshBasicMaterial({ color: 0xff00ff })
-    const cube = new Mesh(geometry, material)
-
-    camera.position.z = 4
+    const clock = new Clock()
+    
+    camera.position.z = 150
     renderer.setClearColor('#1d1d1d')
-    scene.add(cube)
-
     renderer.setSize(width, height)
-    animate({ renderer, scene, camera, cube })
+    renderer.setPixelRatio(window.devicePixelRatio)
+
+    loadBMF({ scene, clock, camera, renderer })
+    console.log('hoge')
+    // animate({ renderer, scene, camera })
   }
 
-  const animate = ({ renderer, scene, camera, cube }: OnCanvasLoaded) => {
-    window.requestAnimationFrame(() => animate({ renderer, scene, camera, cube }))
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+  const loadBMF = ({ scene, clock, camera, renderer }: ParamsLoadBMF) => {
+    loadFont(fontFile, ({err, font}: { err: string, font: any }) => {
+      setGeometry(createGeometry({
+        font,
+        text: 'APOSTRO'
+      }))
+    })
+    
+    setLoader(new TextureLoader())
+    loader.load(fontAtlas, (texture: any) => loadCallback({ scene, clock, camera, renderer, texture }))
+
+    const loadCallback = ({ renderer, scene, camera, texture, clock }: ParamsLoader) => {
+      setTimeout(() => {
+        init({ geometry, texture, scene, clock, camera, renderer })
+        animate({ renderer, scene, camera, clock })
+      })
+    }
+  }
+
+  const init = ({ geometry, texture, scene, clock, camera, renderer, }: ParamsInit) => {
+    createMesh({ geometry, texture, scene })
+    render({ clock, scene, camera, renderer })
+  }
+
+  const createMesh = ({ geometry, texture, scene }: ParamsCreateMesh) => {
+    setMaterial(new RawShaderMaterial(
+      MDFShader({
+        vertexShader: vertex,
+        fragmentShader: fragment,
+        color: '#0xFFFFFF',
+        map: texture,
+        side: DoubleSide,
+        transparent: true,
+        negate: false
+      })
+    ))
+    material.uniforms.time = { type: "f", value: 0.0 }
+
+    setMesh(new Mesh(geometry, material))
+    mesh.position.set(-90, -10, 0)
+    mesh.rotation.set(Math.PI, 0, 0)
+    scene.add(mesh)
+  }
+
+  const animate = ({ renderer, scene, camera, clock }: ParamsAnimate) => {
+    window.requestAnimationFrame(() => animate({ renderer, scene, camera, clock }))
+    render({ clock, scene, camera, renderer })
+  }
+
+  const render = ({ clock, scene, camera, renderer }: ParamsRender) => {
+    mesh.material.uniforms.time.value = clock.getElapsedTime()
+    mesh.material.uniformsNeedUpdate = true
     renderer.render(scene, camera)
   }
 
--- next.config.js
+++ next.config.js
@@ -1,12 +1,26 @@
 const withSass = require('@zeit/next-sass')
 const withImages = require('next-images')
+const withFonts = require('next-fonts')
 
-module.exports = withSass(withImages({
+module.exports = withSass(withImages(withFonts({
   devIndicators: {
     autoPrerender: false,
   },
   webpack(config) {
-    // add opt
+    config.module.rules.push({
+      // shader import support
+      test: /\.glsl$/,
+      use: [
+        {
+          loader: "emit-file-loader",
+          options: {
+            name: "dist/[path][name].[ext]",
+          },
+        },
+        "babel-loader",
+        "webpack-glsl-loader",
+      ],
+    });
     return config
   }
-}))
+})))
