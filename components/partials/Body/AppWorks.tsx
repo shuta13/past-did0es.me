906da0a26e9575010b95f3d47c766dff479f0aa7
shuta13 <hollystarsun@gmail.com>
2020-01-19 23:32:02 +0900

    wip: added AppWorks

--- components/partials/Body/AppWorks.tsx
+++ components/partials/Body/AppWorks.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import './AppWorks.scss'
 
-const AppWorks: React.FC = () => (
-  <div>
+const AppWorks: React.FC<{ img: string, title: string, desc: string }> = ({ img, title, desc }) => (
+  <div className="AppWorksWrap">
+    <div>
+      <img src={img} alt="works" />
+    </div>
+    <div>
+      <div>
+        {title}
+      </div>
+    </div>
+    <div>
+      <div>
+        {desc}
+      </div>
+    </div>
   </div>
 )
 
