701e7907f07eefdf21b80bb0777822dec74c9bf2
shuta13 <hollystarsun@gmail.com>
2020-01-20 03:21:37 +0900

    fix: fixed

--- components/partials/Body/Contents.tsx
+++ components/partials/Body/Contents.tsx
@@ -22,7 +22,7 @@ const WrapContents: React.FC = () => (
         <AppWorks
           img={require('../../../public/static/works/garelly.jpg')}
           title="GARELLY"
-          desc="Site of Generative Art & 2D Graphics."
+          desc="Site of Generative Art & 2D, 3D Graphics."
           link="https://did0es.netlify.com/garelly"
         />
 
@@ -50,7 +50,7 @@ const WrapContents: React.FC = () => (
         <AppWorks
           img={require('../../../public/static/works/vue-tsx-keyframes.jpg')}
           title="vue-tsx-keyframes"
-          desc="JavaScript Library for supporting css of vue-tsx"
+          desc="JavaScript Library for Supporting CSS of Vue-TSX"
           link="https://github.com/shuta13/vue-tsx-keyframes"
         />
 
@@ -64,14 +64,14 @@ const WrapContents: React.FC = () => (
         <AppWorks
           img={require('../../../public/static/works/miko-music.jpg')}
           title="Veginning"
-          desc="music provider 'Veginning' for #VTuberHackPremium"
+          desc="Music Provider 'Veginning' for #VTuberHackPremium"
           link="https://youtu.be/J_CrAM6PEwQ"
         />
 
         <AppWorks
           img={require('../../../public/static/works/towaynight.jpg')}
           title="TO WAY NIGHT"
-          desc="remixed my own music published for university festival"
+          desc="Remixed My Own Music Published for University Festival"
           link="https://soundcloud.com/user-858183512/to-way-night"
         />
 
