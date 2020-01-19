cbd4f18bf2f8e8ed1827cdbf4250a2064d6e8168
shuta13 <hollystarsun@gmail.com>
2020-01-20 07:47:49 +0900

    fix: scroll

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { animateScroll } from 'react-scroll'
 
 import './Header.scss'
 
 const Header: React.FC = () => (
   <div className="HeaderWrap">
-    <img className="HeaderImage" src={require('../../public/static/apostro.svg')} onClick={() => {setTimeout(() => window.scrollTo({ top: 0, behavior: "smooth" }), 100)}}/>
+    <img
+      className="HeaderImage"
+      src={require('../../public/static/apostro.svg')}
+      onClick={
+        () => animateScroll.scrollToTop({
+          smooth: true,
+          duration: 1000
+        })
+      }
+    />
   </div>
 )
 
