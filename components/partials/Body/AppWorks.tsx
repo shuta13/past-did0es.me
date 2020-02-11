17eca1faa71d6c009348487b74725d4498360021
shuta13 <hollystarsun@gmail.com>
2020-02-11 19:52:04 +0900

    fix: for touch device

--- components/partials/Body/AppWorks.tsx
+++ components/partials/Body/AppWorks.tsx
@@ -3,29 +3,35 @@ import './AppWorks.scss'
 
 import ImageCanvas from '../../common/ImageCanvas'
 
-const AppWorks: React.FC<{ img: string, title: string, desc: string, link?: string, subDesc?: string }> = ({ img, title, desc, link, subDesc }) => (
-  <div className="AppWorksWrap">
-    <div className="AppWorksImageWrap">
-      <a href={link} target="_blank" className="AppWorksImageClip">
-        <ImageCanvas img={img} />
-      </a>
-    </div>
-    <div className="AppWorksTitleWrap">
-      <div className="AppWorksTitle">
-        {title}
+import useGetWindowSize from '../../hooks/useGetWindowSize'
+
+const AppWorks: React.FC<{ img: string, title: string, desc: string, link?: string, subDesc?: string }> = ({ img, title, desc, link, subDesc }) => {
+  const { width } = useGetWindowSize()
+  return (
+    <div className="AppWorksWrap">
+      <div className="AppWorksImageWrap">
+        <a href={link} target="_blank" className="AppWorksImageClip">
+          { width > 1000 && <ImageCanvas img={img} /> }
+          { width <= 1000 && <img src={img} className="AppWorksImage" /> }
+        </a>
+      </div>
+      <div className="AppWorksTitleWrap">
+        <div className="AppWorksTitle">
+          {title}
+        </div>
+      </div>
+      <div className="AppWorksDescriptionWrap">
+        <div className="AppWorksDescription">
+          {desc}
+        </div>
+      </div>
+      <div className="AppWorksDescriptionWrap">
+        <div className="AppWorksDescription">
+          {subDesc}
+        </div>
       </div>
     </div>
-    <div className="AppWorksDescriptionWrap">
-      <div className="AppWorksDescription">
-        {desc}
-      </div>
-    </div>
-    <div className="AppWorksDescriptionWrap">
-      <div className="AppWorksDescription">
-        {subDesc}
-      </div>
-    </div>
-  </div>
-)
+  )
+}
 
 export default AppWorks;
