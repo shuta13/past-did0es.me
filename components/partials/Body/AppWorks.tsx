16720952daedc0c06f1d18d51a7c3cd765aeca37
shuta13 <hollystarsun@gmail.com>
2020-02-23 23:04:27 +0900

    wip

--- components/partials/Body/AppWorks.tsx
+++ components/partials/Body/AppWorks.tsx
@@ -4,12 +4,13 @@ import './AppWorks.scss'
 
 import ImageCanvas from '../../common/ImageCanvas'
 
-const AppWorks: React.FC<{ img: string, title: string, desc: string, link?: string, subDesc?: string }> = ({ img, title, desc, link, subDesc }) => {
+const AppWorks: React.FC<{ img: string, title: string, desc: string, link: string, subDesc?: string }> = ({ img, title, desc, link, subDesc }) => {
   const width = window.innerWidth
+  const query = img.split('.')[0].split('/static/works/')[1]
   return (
     <div className="AppWorksWrap">
       <div className="AppWorksImageWrap">
-        <Link href="/details" >
+        <Link href={{ pathname: '/details', query: {title: `${query}`} }} >
           <a className="AppWorksImageClip">
             { width > 615 && <ImageCanvas img={img} /> }
             { width <= 615 && <img src={img} className="AppWorksImage" /> }
