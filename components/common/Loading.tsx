9768cda38a3df732f8d75d5839eba58e2bd690d6
shuta13 <hollystarsun@gmail.com>
2020-03-01 18:07:48 +0900

    wip

---
+++ components/common/Loading.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react'
+
+const Loading: React.FC = () => {
+  const [isLoaded, setIsLoaded] = useState(false)
+  useEffect(() => {
+  }, [])
+  return (
+    <div className={"LoadingWrap" + isLoaded ? "Loaded" : ""}>
+      <div>
+        now loading
+      </div>
+    </div>
+  )
+}
+
+export default Loading
