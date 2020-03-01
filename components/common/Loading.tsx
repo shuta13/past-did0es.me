b91806452ad3bd2e9b9e3b3c9d78e30f4b31c50f
shuta13 <hollystarsun@gmail.com>
2020-03-02 00:37:16 +0900

    wip

--- components/common/Loading.tsx
+++ components/common/Loading.tsx
@@ -6,18 +6,27 @@ import "./Loading.scss";
 const Loading: React.FC = () => {
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
+    const linkEvent = new Event("link");
+    window.addEventListener("link", () => {
+      setIsLoaded(true);
+      document.body.style.overflow = "scroll";
+    });
     document.body.style.overflow = "hidden";
     window.addEventListener("load", () => {
       setIsLoaded(true);
       document.body.style.overflow = "scroll";
     });
-  }, []);
-  Router.events.on("routeChangeComplete", () => {
+    Router.events.on("routeChangeStart", () => {
+      setTimeout(() => {
+        window.dispatchEvent(linkEvent);
+      }, 5000);
+    });
+
+    // Safari... (; ;)
     setTimeout(() => {
       setIsLoaded(true);
-      document.body.style.overflow = "scroll";
-    }, 3600);
-  });
+    }, 5000);
+  }, []);
   return (
     <div className={isLoaded ? "LoadingWrapLoaded" : "LoadingWrap"}>
       {/* now loading */}
