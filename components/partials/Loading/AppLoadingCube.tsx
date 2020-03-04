dc102157d6e7b5640c45f7da7f48aae02831b00d
shuta13 <hollystarsun@gmail.com>
2020-03-05 00:29:42 +0900

    wip

--- components/partials/Loading/AppLoadingCube.tsx
+++ components/partials/Loading/AppLoadingCube.tsx
@@ -4,12 +4,18 @@ import React from "react";
 const AppLoading: React.FC<{ text: string }> = ({ text }) => {
   return (
     <div className="AppLoadingCubeWrap">
-      <div className="AppLoadingCubeTop" />
-      <div className="AppLoadingCubeFront">{text}</div>
-      <div className="AppLoadingCubeBottom" />
+      <div className="AppLoadingCubeTopWrap">
+        <div className="AppLoadingCubeTop" />
+      </div>
+      <div className="AppLoadingCubeFrontWrap">
+        <div className="AppLoadingCubeLeft" />
+        <div className="AppLoadingCubeFront">{text}</div>
+        <div className="AppLoadingCubeRight" />
+      </div>
+      <div className="AppLoadingCubeBottomWrap">
+        <div className="AppLoadingCubeBottom" />
+      </div>
       <div className="AppLoadingCubeBack" />
-      <div className="AppLoadingCubeLeft" />
-      <div className="AppLoadingCubeRight" />
     </div>
   );
 };
