9b37338b2b6cff47b437a9edf3eb96f6f9ea5c2f
shuta13 <hollystarsun@gmail.com>
2020-04-16 05:27:28 +0900

    update

--- components/common/ExternalLink.tsx
+++ components/common/ExternalLink.tsx
@@ -7,15 +7,30 @@ export const ExternalLink: React.FC<{ href: string; text: string }> = ({
 }) => {
   const [isHovered, setIsHovered] = useState(false);
   return (
-    <a
-      href={href}
-      target="_blank"
-      className={isHovered ? "ExternalLinkHovered" : "ExternalLink"}
-      rel="noopener noreferrer"
+    <div
+      className="ExternalLinkWrap"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
+      onTouchStartCapture={() => setIsHovered(true)}
+      onTouchStart={() => setIsHovered(true)}
+      onTouchEndCapture={() => setIsHovered(false)}
+      onTouchEnd={() => setIsHovered(false)}
     >
-      {text}
-    </a>
+      <div
+        className={
+          isHovered ? "ExternalLinkSquareHovered" : "ExternalLinkSquare"
+        }
+      >
+        {!isHovered ? text : null}
+      </div>
+      <a
+        href={href}
+        target="_blank"
+        className={isHovered ? "ExternalLinkHovered" : "ExternalLink"}
+        rel="noopener noreferrer"
+      >
+        {isHovered ? text : null}
+      </a>
+    </div>
   );
 };
