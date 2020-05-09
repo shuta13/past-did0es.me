64260022c7c662786d798b6e1c7a80d84a902a81
shuta13 <hollystarsun@gmail.com>
2020-05-09 16:00:31 +0900

    fix

--- components/common/Loading.tsx
+++ components/common/Loading.tsx
@@ -11,46 +11,46 @@ const Loading: React.FC = () => {
   useEffect(() => {
     const linkEvent = new Event("link");
     // for on load
-    // const startOnLoadAnimation = () => {
-    //   return new Promise(resolve => {
-    //     setIsLoaded(true);
-    //     resolve();
-    //   });
-    // };
-    // // delay for webkit...
-    // const startOnChangeAnimation = () => {
-    //   return new Promise(resolve => {
-    //     setTimeout(() => {
-    //       window.dispatchEvent(linkEvent);
-    //       resolve();
-    //     }, 5000);
-    //   });
-    // };
-    // const finishedAnimate = () => {
-    //   setTimeout(() => {
-    //     setIsFinished(true);
-    //   }, 3000);
-    // };
-    // window.addEventListener("link", () => {
-    //   startOnLoadAnimation().then(() => finishedAnimate());
-    // });
-    // window.addEventListener("load", () => {
-    //   startOnLoadAnimation().then(() => finishedAnimate());
-    // });
-    // Router.events.on("routeChangeStart", () => {
-    //   startOnChangeAnimation().then(() => finishedAnimate());
-    // });
+    const startOnLoadAnimation = () => {
+      return new Promise(resolve => {
+        setIsLoaded(true);
+        resolve();
+      });
+    };
+    // delay for webkit...
+    const startOnChangeAnimation = () => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          window.dispatchEvent(linkEvent);
+          resolve();
+        }, 5000);
+      });
+    };
+    const finishedAnimate = () => {
+      setTimeout(() => {
+        setIsFinished(true);
+      }, 3000);
+    };
+    window.addEventListener("link", () => {
+      startOnLoadAnimation().then(() => finishedAnimate());
+    });
+    window.addEventListener("load", () => {
+      startOnLoadAnimation().then(() => finishedAnimate());
+    });
+    Router.events.on("routeChangeStart", () => {
+      startOnChangeAnimation().then(() => finishedAnimate());
+    });
 
-    // // Safari... (; ;)
-    // const notSupportedAnimation = () => {
-    //   return new Promise(resolve => {
-    //     setTimeout(() => {
-    //       setIsLoaded(true);
-    //       resolve();
-    //     }, 5000);
-    //   });
-    // };
-    // notSupportedAnimation().then(() => finishedAnimate());
+    // Safari... (; ;)
+    const notSupportedAnimation = () => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          setIsLoaded(true);
+          resolve();
+        }, 5000);
+      });
+    };
+    notSupportedAnimation().then(() => finishedAnimate());
   }, []);
   return (
     <div
