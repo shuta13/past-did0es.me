75453e96f6e8c2a38af30a080b43cad3c402a5e2
shuta13 <hollystarsun@gmail.com>
2020-05-11 16:50:25 +0900

    og

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,16 @@ import Contents from "../components/partials/Home/Contents";
 
 const Home: React.FC = () => (
   <>
+    <Head>
+      <meta name="description" content="did0es(shuta13)'s Portfolio" />
+      <meta property="og:site_name" content="did0es.me" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://did0es.me" />
+      <meta property="og:title" content="did0es.me" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="og:description" content="did0es(shuta13)'s Portfolio" />
+      <meta property="og:image" content="https://did0es.me/og-image.png" />
+    </Head>
     <Loading />
     <Contents />
   </>
