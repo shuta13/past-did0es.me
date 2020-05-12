f30b729e6bc2b8685ea27a1f178aad29f6c08080
shuta13 <hollystarsun@gmail.com>
2020-05-12 15:46:43 +0900

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,16 +6,6 @@ import Head from "next/head";
 
 const Home: React.FC = () => (
   <>
-    <Head>
-      <meta name="description" content="did0es(shuta13)'s Portfolio" />
-      <meta property="og:site_name" content="did0es.me" />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://did0es.me" />
-      <meta property="og:title" content="did0es.me" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta property="og:description" content="did0es(shuta13)'s Portfolio" />
-      <meta property="og:image" content="https://did0es.me/og-image.png" />
-    </Head>
     <Contents />
     <Loading />
   </>
