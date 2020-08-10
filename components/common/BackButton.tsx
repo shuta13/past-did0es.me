1a10e3dba0dc2c7c22c1908cd26b724fccc27027
shuta13 <hollystarsun@gmail.com>
2020-08-11 03:06:06 +0900

    fix: some factors

--- components/common/BackButton.tsx
+++ components/common/BackButton.tsx
@@ -1,16 +1,36 @@
 import "./BackButton.scss";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 
 export const BackButton: React.FC<{
   isClicked: boolean;
   setIsClicked: (isClicked: boolean) => void;
 }> = ({ isClicked, setIsClicked }) => {
+  const [isHovered, setIsHovered] = useState(false)
   const text = "< BACK";
   return (
-    <div className="BackButtonWrap">
+    <div
+      className="BackButtonWrap"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      onTouchStartCapture={() => setIsHovered(true)}
+      onTouchStart={() => setIsHovered(true)}
+      onTouchEndCapture={() => setIsHovered(false)}
+      onTouchEnd={() => setIsHovered(false)}
+    >
+        <a 
+          className={
+            isHovered ? "BackButtonSquareHovered" : "BackButtonSquare"
+          }
+          onClick={() => setIsClicked(!isClicked)}>
+          {text}
+        </a>
       <Link href="/">
-        <a className="BackButton" onClick={() => setIsClicked(!isClicked)}>
+        <a 
+          className={
+            isHovered ? "BackButtonHovered" : "BackButton"
+          }
+          onClick={() => setIsClicked(!isClicked)}>
           {text}
         </a>
       </Link>
