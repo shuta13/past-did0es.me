69f48ac0b30ee70f16478880142a9d6e16206e63
shuta13 <hollystarsun@gmail.com>
2020-09-22 21:22:06 +0900

    update

--- components/partials/Home/Social.tsx
+++ components/partials/Home/Social.tsx
@@ -8,12 +8,16 @@ const Social: React.FC = () => (
     <div className="SocialClip">
       <AppTitle title="- Social -" />
       <ExternalLink href="https://twitter.com/did0es" text="Twitter" />
+      <ExternalLink href="https://www.instagram.com/_did0es" text="Instagram" />
+      <ExternalLink href="https://github.com/shuta13" text="GitHub" />
       <ExternalLink
         href="https://www.facebook.com/profile.php?id=100028982675881"
         text="Facebook"
       />
-      <ExternalLink href="https://www.instagram.com/_did0es" text="Instagram" />
-      <ExternalLink href="https://github.com/shuta13" text="GitHub" />
+      <ExternalLink
+        href="https://www.wantedly.com/users/95976645"
+        text="Wantedly"
+      />
       <ExternalLink href="https://scrapbox.io/did0es/" text="Scrapbox" />
     </div>
   </div>
