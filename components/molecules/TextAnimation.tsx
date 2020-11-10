4d85038ba25c52aa349d14fff84cf7328a31abcf
shuta13 <hollystarsun@gmail.com>
2020-11-10 15:07:31 +0900

    wip

--- components/molecules/TextAnimation.tsx
+++ components/molecules/TextAnimation.tsx
@@ -5,16 +5,29 @@ export const TextAnimation: React.FC<{
   isMoveOverlay: boolean;
   text: string;
   fontSize: number;
+  href?: string;
 }> = props => {
-  const { isMoveOverlay, text, fontSize } = props;
+  const { isMoveOverlay, text, fontSize, href } = props;
   return (
     <div className={styles.text_animation}>
-      <div
-        className={isMoveOverlay ? styles.text_moved : styles.text}
-        style={{ fontSize: `${fontSize}rem` }}
-      >
-        {text}
-      </div>
+      {href != null ? (
+        <a
+          className={isMoveOverlay ? styles.text_moved : styles.text}
+          href={href}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ textDecoration: "none" }}
+        >
+          {text}
+        </a>
+      ) : (
+        <div
+          className={isMoveOverlay ? styles.text_moved : styles.text}
+          style={{ fontSize: `${fontSize}rem` }}
+        >
+          {text}
+        </div>
+      )}
     </div>
   );
 };
--- components/partials/Details/DetailsInfo.tsx
+++ components/partials/Details/DetailsInfo.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import styles from "./DetailsInfo.module.scss";
 import { ExternalLink } from "../../common/ExternalLink";
+import { TextAnimation } from "../../molecules/TextAnimation";
 
 type InfoParams = {
   title: string;
@@ -14,30 +15,32 @@ type InfoParams = {
 
 const DetailsInfo: React.FC<{ info: InfoParams }> = ({ info }) => {
   return (
-    <div className={styles.wrap}>
-      <div className={styles.title}>{info.title}</div>
+    <>
+      <div className={styles.wrap}>
+        <TextAnimation isMoveOverlay={true} text={info.title} fontSize={3} />
+        <TextAnimation isMoveOverlay={true} text={info.date} fontSize={1.8} />
+        <TextAnimation isMoveOverlay={true} text={info.tags} fontSize={1.8} />
+        {/* <div className={styles.title}>{info.title}</div>
       <div className={styles.date}>{info.date}</div>
-      <div className={styles.tags}>{info.tags}</div>
-      {/* <div className={styles.description}>
+      <div className={styles.tags}>{info.tags}</div> */}
+        {/* <div className={styles.description}>
         <div
           dangerouslySetInnerHTML={{
             __html: info.desc.replace(/\s/g, "<br />")
           }}
         />
       </div> */}
-      {info.subDesc ? <div className={styles.text}>{info.subDesc}</div> : null}
-      {/* <ExternalLink href={info.link} text="visit" /> */}
-      <div className={styles.link_wrap}>
-        <a
-          href={info.link}
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.link}
-        >
-          VISIT
-        </a>
+        {/* {info.subDesc && <div className={styles.text}>{info.subDesc}</div>} */}
       </div>
-    </div>
+      <div className={styles.link_wrap}>
+        <TextAnimation
+          isMoveOverlay={true}
+          text="VISIT"
+          fontSize={3}
+          href={info.link}
+        />
+      </div>
+    </>
   );
 };
 
