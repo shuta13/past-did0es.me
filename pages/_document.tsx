e6b56c03723058717df14cd0951c6e98c8de76d7
shuta13 <hollystarsun@gmail.com>
2020-12-10 17:04:05 +0900

    fix: og

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,18 +16,26 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="icon" href="/favicon.ico" />
-
-          <meta name="description" content="did0es(shuta13)'s Portfolio" />
-          <meta property="og:site_name" content="did0es.me" />
+          <meta name="description" content="Works, Work Histories of DID0ES" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="did0es.me" />
+          <meta name="twitter:image" content="https://did0es.me/og-image.png" />
+          <meta
+            name="twitter:description"
+            content="Works, Work Histories of DID0ES"
+          />
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://did0es.me" />
           <meta property="og:title" content="did0es.me" />
-          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="og:site_name" content="did0es.me" />
           <meta
             property="og:description"
-            content="did0es(shuta13)'s Portfolio"
+            content="Works, Work Histories of DID0ES"
           />
           <meta property="og:image" content="https://did0es.me/og-image.png" />
+          <meta property="og:image:type" content="image/png" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
         </Head>
         <body>
           <Main />
