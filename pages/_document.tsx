156bc901b62cada07c916131329e5eda7e9e5a09
shuta13 <hollystarsun@gmail.com>
2021-02-02 15:43:55 +0900

    chore: json ld

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,37 @@ import Document, {
   DocumentContext
 } from "next/document";
 
+interface JsonLdParams {
+  title: string;
+  updated: string;
+  url: string;
+  imageUrl: string;
+  description: string;
+}
+
+// @see https://github.com/vercel/next.js/issues/2213#issuecomment-307478160
+const jsonLd = ({
+  title,
+  updated,
+  url,
+  imageUrl,
+  description
+}: JsonLdParams) => {
+  const json = {
+    "@context": "http://schema.org",
+    "@type": "Website",
+    name: title,
+    headline: title,
+    datePublished: "2021-02-02T00:00:00.000Z",
+    dateModified: updated,
+    url: url,
+    mainEntityOfPage: url,
+    image: [imageUrl],
+    description: description
+  };
+  return JSON.stringify(json);
+};
+
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -36,6 +67,19 @@ class MyDocument extends Document {
           <meta property="og:image:type" content="image/png" />
           <meta property="og:image:width" content="1200" />
           <meta property="og:image:height" content="630" />
+
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{
+              __html: jsonLd({
+                title: "did0es.me",
+                description: "Works, Work Histories of DID0ES",
+                imageUrl: "https://did0es.me/og-image.png",
+                url: "https://did0es.me",
+                updated: new Date().toISOString()
+              })
+            }}
+          />
         </Head>
         <body>
           <Main />
