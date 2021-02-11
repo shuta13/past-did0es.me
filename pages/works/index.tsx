cec05b7e89d3174b7ed50dc44b9d9c6db56cc945
shuta13 <hollystarsun@gmail.com>
2021-02-11 18:21:24 +0900

    fix: fix

--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -1,12 +1,15 @@
+import { GetStaticProps } from "next";
 import React, { useEffect } from "react";
 import { Carousel } from "../../components/organisms/Carousel";
+import { Response } from "../../shared/types/Response";
 
 const Work: React.FC<{
   setIsMenuReset: (isMenuReset: boolean) => void;
   setIsWorksActive: (isWorksActive: boolean) => void;
   setIsContactActive: (isContactActive: boolean) => void;
+  data: Response["data"];
 }> = props => {
-  const { setIsMenuReset, setIsWorksActive, setIsContactActive } = props;
+  const { setIsMenuReset, setIsWorksActive, setIsContactActive, data } = props;
 
   useEffect(() => {
     setIsWorksActive(true);
@@ -15,9 +18,17 @@ const Work: React.FC<{
 
   return (
     <div className="container" style={{ overflow: "hidden" }}>
-      <Carousel setIsMenuReset={setIsMenuReset} />
+      {data.length > 0 && (
+        <Carousel setIsMenuReset={setIsMenuReset} data={data} />
+      )}
     </div>
   );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://did0es.me/api/v1");
+  const data: Response["data"] = await res.json();
+  return { props: { data } };
 };
 
 export default Work;
