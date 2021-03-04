5d2fcb43274de1acb79d3eab010d045dd2ef722a
shuta13 <hollystarsun@gmail.com>
2021-03-05 00:27:54 +0900

    rebert

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export const END_POINT_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://did0es.me";
+    : "https://60409aea4eebaf0008b4fd04--vigilant-wescoff-d7fab5.netlify.app";
